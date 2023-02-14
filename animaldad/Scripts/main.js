172ea1b636083e3ebc3e72683f7c9057483cc5ff
MDMGN <michaelmdvrhack@gmail.com>
2023-02-14 11:16:08 +0100

    Agregar archivos de proyecto.

---
+++ animaldad/Scripts/main.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded',()=>{
+    //Agregar la clase active
+    const $nav = document.querySelector(".nav-tabs");
+    const listItems = $nav.querySelectorAll("li");
+    console.log(location.href.split("/"))
+    listItems.forEach(function (listItem) {
+        const currentURL = location.href.split("/")[3];
+        const target = currentURL.toLowerCase()
+        if (target==listItem.id)  listItem.className = "active"
+    });
+});
